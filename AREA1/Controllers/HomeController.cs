ddecc0db56849fff06871d297ca85a6452a478a5
elliot_kjw <eidechse4@hanmail.net>
2021-04-05 13:09:12 +0900

    디비 테스트 페이지 작성

--- AREA1/Controllers/HomeController.cs
+++ AREA1/Controllers/HomeController.cs
@@ -36,8 +36,26 @@ namespace AREA1.Controllers {
 
             transaction.Commit();
 
-
             return View();
+        }
+
+        public IActionResult InsertData() {
+            using var transaction = _context.Database.BeginTransaction();
+
+
+            string query = "INSERT INTO PERSONS VALUES(" + Request.Form["person_id"] + ","
+                                                          + "'" + Request.Form["last_name"] + "',"
+                                                          + "'" + Request.Form["first_name"] + "',"
+                                                          + "'" + Request.Form["address"] + "',"
+                                                          + "'" + Request.Form["city"]
+                                                          + "')";
+
+
+            _commonDao.Insert(query);
+
+            transaction.Commit();
+
+            return Redirect("Index");
         }
 
         public IActionResult Privacy() {
