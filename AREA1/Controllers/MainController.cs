85b35881d0530d9b9060d30eaef6c709df9adeb6
elliot_kjw <eidechse4@hanmail.net>
2021-05-11 20:27:10 +0900

    메인 수정

--- AREA1/Controllers/MainController.cs
+++ AREA1/Controllers/MainController.cs
@@ -11,24 +11,19 @@ using Tool;
 
 namespace AREA1.Controllers {
     [LoginActionFilter]
-    public class MainController : Controller
-    {
+    public class MainController : Controller {
         private readonly ILogger<MainController> _logger;
         private readonly AppSoftDbContext _context;
         private readonly CommonDao _commonDao;
-        public MainController(ILogger<MainController> logger, AppSoftDbContext context)
-        {
+        public MainController(ILogger<MainController> logger, AppSoftDbContext context) {
             _logger = logger;
             _context = context;
             _commonDao = new CommonDao(context);
         }
-        public IActionResult Main()
-        {
+        public IActionResult Main() {
             UserModel userInfo = SessionExtensionTool.GetObject<UserModel>(HttpContext.Session, "userInfo");
-<<<<<<< HEAD
 
-            if (userInfo == null)
-            {
+            if (userInfo == null) {
                 return RedirectToAction("/Login", new { alertLogin = 2 });
             }
 
@@ -62,21 +57,20 @@ namespace AREA1.Controllers {
                 ViewBag.ResultList = resultList;
                 ViewBag.ResultCount = resultList.Count;
             }
-            
+
             return View("/Views/Main.cshtml");
         }
-=======
->>>>>>> 5dfa0c7a11bb13c7ca17b80bbc9ca744963d6425
 
-            if (userInfo == null)
-            {
-                return RedirectToAction("/Login", new { alertLogin = 2 });
-            }
+        public List<Dictionary<string, List<Dictionary<string, string>>>> YearhakgiAtnlcSbjectList() {
+            UserModel userInfo = SessionExtensionTool.GetObject<UserModel>(HttpContext.Session, "userInfo");
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            // 학기 당 과목리스트
+            List<Dictionary<string, string>> subjList = null;
+            // 학기 정보 리스트
+            List<Dictionary<string, List<Dictionary<string, string>>>> resultList = new List<Dictionary<string, List<Dictionary<string, string>>>>();
 
-            ViewData["name"] = userInfo.name;
-            ViewData["user_id"] = userInfo.user_id;
+            param["USER_ID"] = userInfo.user_id;
 
-<<<<<<< HEAD
             if (userInfo.author.Equals("1000")) {
                 string sql = "SELECT B.ACDMC_NO, A.SEMESTER, A.YEAR, D.TITLE || ' (' || B.ACDMC_NO || ') - ' || C.NAME AS LABEL"
                            + " FROM OP_TEACHES A"
@@ -90,37 +84,16 @@ namespace AREA1.Controllers {
                            + " AND A.semester = '1'"
                            + " AND A.YEAR = '2021'"
                            + " ORDER BY A.COURSE_ID";
-=======
-            ViewData["Title"] = HttpContext.Session.GetString("_Key");
->>>>>>> 5dfa0c7a11bb13c7ca17b80bbc9ca744963d6425
 
-            string sql = "SELECT COUNT(*) AS TAKES_CNT"
-                + " FROM OP_TAKES"
-                + " WHERE ID=" + userInfo.user_id;
 
-            int takes_cnt = 0;
-            takes_cnt = Convert.ToInt32(_commonDao.SelectOne(sql)["TAKES_CNT"]);
+                Dictionary<string, List<Dictionary<string, string>>> result = new Dictionary<string, List<Dictionary<string, string>>>();
 
-            if (takes_cnt > 0) {
-                sql = "SELECT TITLE,NAME,DAY1,DAY2,PERIOD1,PERIOD2,BUILDING,ROOM_NUMBER,"
-                    + "(SELECT NAME FROM OP_USER JOIN OP_ADVISOR ON OP_USER.USER_ID=OP_ADVISOR.I_ID WHERE OP_ADVISOR.S_ID=A.ID) AS ADVISOR, "
-                    + "(SELECT EMAIL FROM OP_USER JOIN OP_ADVISOR ON OP_USER.USER_ID=OP_ADVISOR.I_ID WHERE OP_ADVISOR.S_ID=A.ID) AS AD_EMAIL,"
-                    + "(SELECT PHONE FROM OP_USER JOIN OP_ADVISOR ON OP_USER.USER_ID=OP_ADVISOR.I_ID WHERE OP_ADVISOR.S_ID=A.ID) AS AD_PHONE "
-                   + "FROM OP_TAKES A "
-                   + "JOIN OP_TEACHES B "
-                   + "ON A.SEC_ID=B.SEC_ID AND A.COURSE_ID=B.COURSE_ID AND A.SEMESTER=B.SEMESTER AND A.YEAR=B.YEAR "
-                   + "JOIN OP_SECTION C "
-                   + "ON A.SEC_ID=C.SEC_ID AND A.COURSE_ID=C.COURSE_ID AND A.SEMESTER=C.SEMESTER AND A.YEAR=C.YEAR "
-                   + "JOIN OP_COURSE D ON A.COURSE_ID=D.COURSE_ID "
-                   + "JOIN OP_USER E ON E.USER_ID=B.ID "
-                   + "JOIN OP_TIME_SLOT F on C.TIME_SLOT_ID = F.TIME_SLOT_ID ";
+                subjList = _commonDao.SelectList(sql);
+                result["subjList"] = subjList;
 
-                var resultList = _commonDao.SelectList(sql);
-                ViewBag.ResultList = resultList;
-                ViewBag.ResultCount = resultList.Count;
+                resultList.Add(result);
             }
-            
-            return View("/Views/Main.cshtml");
+            return resultList;
         }
     }
 }
