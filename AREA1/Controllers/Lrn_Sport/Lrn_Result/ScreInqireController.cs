9ce25cae5429a7f097a36c75c78358bc00f7c7d3
jaehawilly <jaehawilly@naver.com>
2021-06-01 00:28:51 +0900

    jaeha0601 temp

--- AREA1/Controllers/Lrn_Sport/Lrn_Result/ScreInqireController.cs
+++ AREA1/Controllers/Lrn_Sport/Lrn_Result/ScreInqireController.cs
@@ -8,7 +8,7 @@ using System;
 using System.Collections.Generic;
 using Tool;
 
-namespace AREA1.Controllers
+namespace AREA1.Controllers.Lrn_Result.SCRE_INQIRE
 {
     [LoginActionFilter]
     public class ScreInqireController : Controller
